cf18b439bb49cc1f521fbe4ec32619ef704f0224
enayetsyl <enayetflweb@gmail.com>
2025-04-08 09:22:58 +0600

    import corrected

--- frontend/components/ui/checkbox.tsx
+++ frontend/components/ui/checkbox.tsx
@@ -3,8 +3,9 @@
 import * as React from "react"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
 import { CheckIcon } from "lucide-react"
+import { cn } from "lib/utils"
 
-import { cn } from "@/lib/utils"
+
 
 function Checkbox({
   className,
