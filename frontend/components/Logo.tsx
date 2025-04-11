e3997a8220f1c8cba7986ecc52b36eb16e8be7fb
SougataMondal2000 <sougataofficial362000@gmail.com>
2025-04-11 09:05:23 +0530

    UI fixes

--- frontend/components/Logo.tsx
+++ frontend/components/Logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Logo = () => {
   return (
-    <div className="hidden md:flex">
+    <div className="md:flex">
       <Image src="/logo.svg" alt="logo" height={60} width={180} />
     </div>
   );
