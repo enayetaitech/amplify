b0a97f5c1bbddf7aa9f89318b2591e2155b3037e
enayetsyl <enayetflweb@gmail.com>
2025-05-16 11:42:08 +0600

    tag add and delete integration done

--- frontend/components/viewProject/CreateTagModal.tsx
+++ frontend/components/viewProject/CreateTagModal.tsx
@@ -8,7 +8,6 @@ import {
   DialogTitle,
   DialogFooter,
 } from "components/ui/dialog";
-import { Button } from "components/ui/button";
 import { Input } from "components/ui/input";
 import { Badge } from "components/ui/badge";
 import { ApiResponse } from "@shared/interface/ApiResponseInterface";
