410b628fdb3d52b3b34240d4f7e6a6cff60f8e99
elprofessor67 <jeeshanr599@gmail.com>
2025-05-24 00:09:30 +0530

    Add docker

--- backend/controllers/ModeratorController.ts
+++ backend/controllers/ModeratorController.ts
@@ -1,12 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import ModeratorModel, { IModeratorDocument } from "../model/ModeratorModel";
-import { sendResponse } from "../utils/ResponseHelpers";
+import { sendResponse } from "../utils/responseHelpers";
 import ErrorHandler from "../../shared/utils/ErrorHandler";
 import ProjectModel from "../model/ProjectModel";
 import User from "../model/UserModel";
 
 import config from "../config";
-import { sendEmail } from "../processors/sendEmail/SendVerifyAccountEmailProcessor";
+import { sendEmail } from "../processors/sendEmail/sendVerifyAccountEmailProcessor";
 import { moderatorAddedEmailTemplate } from "../constants/emailTemplates";
 import mongoose, { Types } from "mongoose";
 
