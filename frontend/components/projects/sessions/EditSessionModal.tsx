2448081a40325c65ddb8126b5c96521dc5e1cc9b
enayetsyl <enayetflweb@gmail.com>
2025-05-26 10:02:18 +0600

    edit session modal fixed and add session invalidate query fixed

--- frontend/components/projects/sessions/EditSessionModal.tsx
+++ frontend/components/projects/sessions/EditSessionModal.tsx
@@ -90,7 +90,7 @@ export default function EditSessionModal({
     defaultValues: {
       title: session?.title ?? "",
       date: session
-        ? session.date.toISOString().slice(0, 10)
+        ? new Date(session.date).toISOString().slice(0, 10)
         : new Date().toISOString().slice(0, 10),
       startTime: session?.startTime ?? "",
       duration: session?.duration ?? 30,
