0fb785eb95925e35c29388b941f0dd25d08e4fe6
enayetsyl <enayetflweb@gmail.com>
2025-05-27 19:21:40 +0600

    login page set as home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,23 +1,10 @@
-import HeadingBlue25px from "components/HeadingBlue25pxComponent";
-import { Button } from "components/ui/button";
-import Link from "next/link";
+import Login from 'components/login/login'
+import React from 'react'
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen meeting_bg space-y-5">
-      <HeadingBlue25px>Welcome to Amplify Research</HeadingBlue25px>
-
-      <Link href="/login">
-        <Button variant="default" className="bg-custom-orange-1 hover:bg-custom-orange-2">
-          Login
-        </Button>
-      </Link>
-
-      <Link href="/create-user">
-        <Button variant="default" className="bg-[#FC6E15] hover:bg-[#FC65E15]">
-          Register
-        </Button>
-      </Link>
-    </div>
-  );
+    <Login/>
+  )
 }
+
+export default page
