15c25ef768e7dfab30acdf1a83b72a930c165e83
enayetsyl <enayetflweb@gmail.com>
2025-06-06 15:06:34 +0600

    use project refactored

--- frontend/app/(dashboard)/view-project/[id]/page.tsx
+++ frontend/app/(dashboard)/view-project/[id]/page.tsx
@@ -1,24 +1,16 @@
 "use client";
 
-import { ApiResponse } from "@shared/interface/ApiResponseInterface";
-import { IProject } from "@shared/interface/ProjectInterface";
-import { useQuery } from "@tanstack/react-query";
 import HeadingBlue25px from "components/HeadingBlue25pxComponent";
 import ComponentContainer from "components/shared/ComponentContainer";
 import CreditSummary from "components/viewProject/CreditSummary";
 import ProjectSummary from "components/viewProject/ProjectSummary";
 import SessionAccess from "components/viewProject/SessionAccess";
 import TagModal from "components/viewProject/TagModel";
-import api from "lib/api";
+import { useProject } from "hooks/useProject";
+
 import { useParams } from "next/navigation";
 import React, { useState } from "react";
 
-const fetchProjectById = async (id: string) => {
-  const res = await api.get<ApiResponse<IProject>>(
-    `/api/v1/projects/get-project-by-id/${id}`
-  );
-  return res.data.data;
-};
 
 const ViewProject = () => {
   const { id: projectId } = useParams() as { id: string };
@@ -26,15 +18,10 @@ const ViewProject = () => {
 
   const {
     data: project,
-    isPending: isLoading,
+    isLoading,
     isError,
     error,
-  } = useQuery({
-    queryKey: ["project", projectId],
-    queryFn: () => fetchProjectById(projectId),
-    enabled: !!projectId,
-  });
-
+  } = useProject(projectId);
  
 
   if (isLoading) return <p>Loading project…</p>;
@@ -45,12 +32,11 @@ const ViewProject = () => {
     <ComponentContainer>
       <div className=" py-5 ">
         <HeadingBlue25px>Project Details: {project!.name}</HeadingBlue25px>
-        {/* two-column grid on md+ */}
+       
         <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6">
-          {/* Project Summary */}
+       
           <ProjectSummary
             project={project!}
-         
             onTagEditClick={() => setIsTagModalOpen(true)}
           />
 
