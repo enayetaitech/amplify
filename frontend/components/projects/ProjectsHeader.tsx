b17bd21cc24f0528af8cbedb00b77247a7ec7eec
enayetsyl <enayetflweb@gmail.com>
2025-06-06 12:52:47 +0600

    project header, project filter, useProject hook refactored

---
+++ frontend/components/projects/ProjectsHeader.tsx
@@ -0,0 +1,29 @@
+// frontend/components/projects/ProjectsHeader.tsx
+"use client";
+
+import React from "react";
+import { Plus } from "lucide-react";
+import HeadingBlue25px from "components/HeadingBlue25pxComponent";
+import CustomButton from "components/shared/CustomButton";
+
+
+interface ProjectsHeaderProps {
+  onCreateClick: () => void;
+}
+
+const ProjectsHeader: React.FC<ProjectsHeaderProps> = ({ onCreateClick }) => {
+  return (
+    <div className="flex justify-between items-center mb-6">
+      <HeadingBlue25px>Projects Dashboard</HeadingBlue25px>
+      <CustomButton
+        icon={<Plus />}
+        className="bg-custom-orange-1 hover:bg-custom-orange-2 text-custom-white"
+        onClick={onCreateClick}
+      >
+        Create New Project
+      </CustomButton>
+    </div>
+  );
+};
+
+export default ProjectsHeader;
