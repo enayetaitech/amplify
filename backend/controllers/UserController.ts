1c5e9eb7c8207f90d75a9245d20bddcd9f4a7089
enayetsyl <enayetflweb@gmail.com>
2025-07-25 11:53:22 +0600

    user controller import corrected and docker compose file edited

--- backend/controllers/UserController.ts
+++ backend/controllers/UserController.ts
@@ -24,7 +24,7 @@ import {
 } from "../utils/tokenService";
 import { AuthRequest } from "../middlewares/authenticateJwt";
 import { Types } from "mongoose";
-import { isValidEmail } from "processors/isValidEmail";
+import { isValidEmail } from "../processors/isValidEmail";
 
 export const createAccount = async (
   req: Request,
