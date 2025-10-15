092b06f96074eb84ef125a7962ad8084698772ef
enayetsyl <enayetflweb@gmail.com>
2025-10-15 15:18:51 +0600

    Feat(Report): Observer count and details display

--- shared/dist/interface/LiveSessionInterface.d.ts
+++ shared/dist/interface/LiveSessionInterface.d.ts
@@ -40,6 +40,7 @@ export interface IObserverHistoryItem {
     role: Extract<UserRole, "Observer" | "Moderator" | "Admin">;
     joinedAt?: Date | null;
     leaveAt?: Date | null;
+    reason?: "Left" | "Streaming Stopped";
 }
 export interface ILiveSession {
     _id: string;
